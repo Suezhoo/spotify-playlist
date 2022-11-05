5a37102c7b140265ffc61398306bf2a88bcf08da
Suezhoo <suezhoo@outlook.com>
2022-11-05 04:09:13 +0100

    [WIP] get playlist by id and display data

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ server.get('/', (req, res) => {
     res.status(300).redirect('/index.html');
 });
 
+/**
+ *  Retrieves access token from https://accounts.spotify.com/api/token
+ *
+ * @returns Bearer access token
+ */
 server.get('/token', async (req, res) => {
     try {
         const client_id = process.env.CLIENT_ID;
@@ -38,13 +43,39 @@ server.get('/token', async (req, res) => {
             json: true,
         })
             .then(function (response) {
-                res.status(200).send(response.data);
+                if (response.status == 200) res.status(200).send(response.data);
+                else res.status(500).send('Something went wrong.');
             })
             .catch(function (error) {
                 console.log(error);
             });
     } catch (err) {
         console.log(err);
+    }
+});
+
+/**
+ * Gets playlist based on ID
+ *
+ * @params id - Id of the playlist
+ * @headers authorization - spotify access token
+ * @returns playlist
+ *
+ */
+server.get('/playlist', async (req, res) => {
+    try {
+        await axios({
+            method: 'GET',
+            url: 'https://api.spotify.com/v1/playlists/' + req.query.id,
+            headers: {
+                Authorization: 'Bearer ' + req.headers.authorization,
+                'Content-Type': 'application/json',
+            },
+        }).then(function (response) {
+            if (response.status == 200) res.status(200).send(response.data);
+        });
+    } catch (err) {
+        res.status(err.response.status).send(err.response.statusText);
     }
 });
 
--- public/script.js
+++ public/script.js
@@ -1,19 +1,98 @@
 'use strict;';
 
+let token;
+
+// Playlist ids
+// 4NepCfIQDBifrAKHCuTnJq?si=25cc2fd13d104cad
+// 5AGEjbB4biEBlSXDhUI2ZU?si=74d6bcc8595541e0
+
 window.onload = async () => {
-    console.log('Hello World from JS');
-
-    const token = await getToken();
-
-    console.log(token);
+    initHTML();
+    initEventListeners();
+    token = await getToken();
+    token = token.access_token;
 };
+
+function initHTML() {
+    document.querySelector('.playlist-info').innerHTML = `<h1 class="title">Playlist Backup</h1>
+    <p class="description">Want to backup your playlists? All we need is the playlist ID and we will export your tracks into a single file.</p>
+    <input type="text" name="playlist-id" id="playlist-id" placeholder="Playlist ID" required autofocus value='4NepCfIQDBifrAKHCuTnJq?si=25cc2fd13d104cad'/>
+    <div class="button" id="submit">Submit</div>`;
+}
+
+function initEventListeners() {
+    const submitBTN = document.querySelector('#submit');
+
+    submitBTN.addEventListener('click', () => {
+        const playlistID = document.querySelector('#playlist-id').value;
+        if (playlistID == '') console.log('Wrong id');
+        else getPlaylist(playlistID, token);
+    });
+}
 
 /**
  *
- * @returns Bearer token
+ * @returns {*} Bearer token
  */
 async function getToken() {
     return fetch('http://localhost:1337/token', {
         method: 'GET',
     }).then((res) => res.json());
+}
+
+/**
+ *
+ *
+ * @param {*} id
+ * @param {*} token
+ */
+async function getPlaylist(id, token) {
+    fetch('http://localhost:1337/playlist/?id=' + id, {
+        method: 'GET',
+        headers: {
+            authorization: token,
+        },
+    })
+        .then((res) => {
+            if (res.status == 200) return res.json();
+            else console.log(res);
+        })
+        .then((data) => {
+            document.querySelector('.playlist-info').innerHTML = `
+            <div class="result">
+                <div class="playlist-card">
+                    <img src="${data.images[0].url}" alt="album-picture" />
+                    <div class="details">
+                        <p>${data.type.toUpperCase()}</p>
+                        <h1>${data.name}</h1>
+                        <p>${data.owner.display_name} ~ ${data.followers.total} likes ~ ${data.tracks.total.toLocaleString()} songs</p>
+                    </div>
+                </div>
+                <table class="tracks">
+                    <tbody class="tracks-body">
+                        <tr class="th-underline">
+                            <th class="first">#</th>
+                            <th>Title</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>`;
+
+            // Display x amount of tracks where => x = for(let i = 0; i < X; i++)
+            for (let i = 0; i < 6; i++) {
+                let rowString = `
+                <tr>
+                    <td>${i + 1}</td>
+                    <td class="track-content">
+                        <img src="${data.tracks.items[i].track.album.images[data.tracks.items[i].track.album.images.length - 1].url}" alt="thumbnail"></img> 
+                        <span>${data.tracks.items[i].track.name}</span>
+                    </td>
+                </tr>`;
+                document.querySelector('.tracks-body').insertAdjacentHTML('beforeend', rowString);
+            }
+
+            const button = `<div class="button" id="export">Export</div>`;
+
+            document.querySelector('.playlist-info').insertAdjacentHTML('beforeend', button);
+        });
 }
